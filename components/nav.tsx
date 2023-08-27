d58b02977cb06e2a44a6eadb729642da05eb6ad2
SamuelLo <samuello300@gmail.com>
2023-08-27 15:06:26 -0700

    Iitializing application

---
+++ components/nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'; //react component to provide client side navigation between routes
+import Image from 'next/image'; //use for logo
+
+
+const nav = () => {
+  return (
+    <div>nav</div>
+  )
+}
+
+export default nav
