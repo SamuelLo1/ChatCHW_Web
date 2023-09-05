9003501551421cb0df2c126ba5ddc5f43e7c92db
Dominier <118577440+Dominier@users.noreply.github.com>
2023-09-05 16:23:11 -0700

    Import global.css - DO NOT REMOVE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import NavigationBar from "@/components/navbar";
+import './globals.css';
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
+
+import NavigationBar from "@/components/navbar";
 
 const inter = Inter({ subsets: ['latin']})
 
