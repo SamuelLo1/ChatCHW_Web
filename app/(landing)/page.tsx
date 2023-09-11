9c279be404ec78685c85244697fbe18524d2235c
SamuelLo <samuello300@gmail.com>
2023-09-10 21:02:56 -0700

    Design changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -20,14 +20,14 @@ const HomePage = () => {
       <div className="flex items-center justify-evenly">
         {/*SlideShow*/ }
 
-        <div className="max-w-[800px] h-[600px] py-16 px-4"> 
+        <div className="max-w-[650px] h-[450px] py-16 px-4 "> 
         
             <Image
             src={slides[1].url}
-            height={800}
-            width={600}
+            height={600}
+            width={400}
             alt = "image"
-            className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+            className=' border-8 border-orange-500 w-full h-full bg-center bg-cover duration-500 '
 
           />
   
@@ -35,13 +35,25 @@ const HomePage = () => {
 
 
         <div className="align-middle flex-wrap ml-4 w-4/12 overflow-wrap " > 
-           <h1 className="font-semibold	text-6xl tracking-wide">Empowering community health workers</h1>
+           <h1 className="	text-7xl tracking-wide text-gray-800 ">Empowering community health workers</h1>
         </div> 
 
+         
       </div>
-    </div>
+      <div className ="w-full "> 
+          <section className="text-center px-36 text-5xl font-bold text-gray-700">
+            <h1> Facilitating Community</h1>
+          </section>
 
+          <section className="text-gray-600 text-center py-16 px-72 flex-wrap ">
+            <p>Delivering innovative solutions and fostering collaboration to achieve lasting positive impact and sustainable growth. 
+              We offer a comprehensive platform that equips CHWs with cutting-edge technology and training, enabling them to deliver 
+              more efficient and effective care to their communities.</p>
+          </section>
+      </div> 
+    </div>
+    
   )
-} 
+}
 
 export default HomePage;
