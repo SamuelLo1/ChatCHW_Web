664a2aa703d074ffb2ba92dde7031491b9d482ca
SamuelLo <samuello300@gmail.com>
2023-09-15 12:07:14 -0700

    loaded in home svg

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -20,14 +20,14 @@ const HomePage = () => {
       <div className="flex items-center justify-evenly">
         {/*SlideShow*/ }
 
-        <div className="max-w-[650px] h-[450px] py-16 px-4 "> 
-        
+        <div className="relative max-w-[650px] h-[480px] py-16 px-4 "> 
+            
             <Image
-            src={slides[1].url}
-            height={600}
-            width={400}
+            src={"/assets/home.svg"}
+            height={379}
+            width={347}
             alt = "image"
-            className=' border-8 border-orange-500 w-full h-full bg-center bg-cover duration-500 '
+            className='bg-center bg-cover'
 
           />
   
@@ -35,17 +35,17 @@ const HomePage = () => {
 
 
         <div className="align-middle flex-wrap ml-4 w-4/12 overflow-wrap " > 
-           <h1 className="	text-7xl tracking-wide text-gray-800 ">Empowering community health workers</h1>
+           <h1 className="	text-7xl tracking-wide text-gray-600 ">Empowering community health workers</h1>
         </div> 
 
          
       </div>
       <div className ="w-full "> 
-          <section className="text-center px-36 pt-6 text-5xl font-bold text-gray-700">
+          <section className="text-center px-36 pt-6 text-5xl font-bold text-gray-600">
             <h1> Facilitating Community</h1>
           </section>
 
-          <section className="text-gray-600 text-center py-16 px-96 flex-wrap ">
+          <section className="text-gray-400 text-center py-16 px-96 flex-wrap ">
             <p>Delivering innovative solutions and fostering collaboration to achieve lasting positive impact and sustainable growth. 
               We offer a comprehensive platform that equips CHWs with cutting-edge technology and training, enabling them to deliver 
               more efficient and effective care to their communities.</p>
@@ -55,5 +55,5 @@ const HomePage = () => {
     
   )
 }
-
+//add overlay on bottom images
 export default HomePage;
