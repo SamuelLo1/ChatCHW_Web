fa4563ef27ba26804f1a030bc6853046e37f71ef
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-09-19 14:57:38 -0700

    Added text

--- app/(dashboard)/(routes)/about/page.tsx
+++ app/(dashboard)/(routes)/about/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 const AboutPage = () => {
   return (
     <div>
-        <div>About Page</div>
+        {/* Title Text */}
+        <div>
+          <h1 className="font-bold">
+            Why people love ChatCHW
+          </h1>
+        </div>
+
+        {/* Paragraph Text */}
+        <div>
+          <p>
+            At ChatCHW, we measure our success by the <br />
+            positive outcomes achieved by CHWs and the <br />
+            communities they serve. By equipping CHWs with <br />
+            the resources and support they need, we aim to: <br />
+          </p>
+        </div>
+
+        {/* Bullet points */}
+        <div>
+          <ul className="list-outside">
+            <li>Improve healthcare access and quality in underserved communities</li>
+            <li>Reduce health disparities and imrpove health equity</li>
+            <li>Enchance the overall well-being and health outcomes of individuals and families</li>
+            <li>Strengthen community resilience and self-sufficiency</li>
+          </ul>
+        </div>
+
+        {/* Boxes */}
+
+
     </div>
     
   )
