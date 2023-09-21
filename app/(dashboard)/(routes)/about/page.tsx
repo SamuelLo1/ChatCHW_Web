26d16593c31df0bac3b19468d403d69202c46ad5
Dominier <118577440+TimCooks@users.noreply.github.com>
2023-09-20 17:45:15 -0700

    Left Side Placement

--- app/(dashboard)/(routes)/about/page.tsx
+++ app/(dashboard)/(routes)/about/page.tsx
@@ -3,35 +3,7 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-        {/* Title Text */}
-        <div>
-          <h1 className="font-bold">
-            Why people love ChatCHW
-          </h1>
-        </div>
-
-        {/* Paragraph Text */}
-        <div>
-          <p>
-            At ChatCHW, we measure our success by the <br />
-            positive outcomes achieved by CHWs and the <br />
-            communities they serve. By equipping CHWs with <br />
-            the resources and support they need, we aim to: <br />
-          </p>
-        </div>
-
-        {/* Bullet points */}
-        <div>
-          <ul className="list-outside">
-            <li>Improve healthcare access and quality in underserved communities</li>
-            <li>Reduce health disparities and imrpove health equity</li>
-            <li>Enchance the overall well-being and health outcomes of individuals and families</li>
-            <li>Strengthen community resilience and self-sufficiency</li>
-          </ul>
-        </div>
-
-        {/* Boxes */}
-
+        <p>About Page</p>
 
     </div>
     
--- app/(dashboard)/(routes)/blog/page.tsx
+++ app/(dashboard)/(routes)/blog/page.tsx
@@ -1,7 +1,41 @@
 const BlogPage = () => {
     return ( 
         <div>
-            Blog Page
+            {/* Left */}
+            <div className="border border-black w-[35%] ml-[10%] mt-[5%]">
+                {/* Title Text */}
+                <div>
+                    <h1 className="font-bold">
+                        Why people love ChatCHW
+                    </h1>
+                </div>
+
+                {/* Paragraph Text */}
+                <div>
+                    <p>
+                        At ChatCHW, we measure our success by the <br />
+                        positive outcomes achieved by CHWs and the <br />
+                        communities they serve. By equipping CHWs with <br />
+                        the resources and support they need, we aim to: <br />
+                    </p>
+                </div>
+
+                {/* Bullet points */}
+                <div>
+                    <ul className="list-outside">
+                        <li>Improve healthcare access and quality in underserved communities</li>
+                        <li>Reduce health disparities and imrpove health equity</li>
+                        <li>Enchance the overall well-being and health outcomes of individuals and families</li>
+                        <li>Strengthen community resilience and self-sufficiency</li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Boxes (Right) */}
+            <div>
+                
+            </div>
+
         </div>
      );
 }
