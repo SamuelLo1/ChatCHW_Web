3110f80ddc4a18af9d05a69b5dedf3b65a3837a3
Timothy Le <tle0279@gmail.com>
2023-09-26 16:05:05 -0700

    Color format and spacing

--- app/(dashboard)/(routes)/blog/page.tsx
+++ app/(dashboard)/(routes)/blog/page.tsx
@@ -2,31 +2,31 @@ const BlogPage = () => {
     return ( 
         <div>
             {/* Left */}
-            <div className="border border-black w-[35%] ml-[10%] mt-[5%]">
+            <div className="border border-black w-[45%] ml-[10%] mt-[5%] text-[#505D68]">
                 {/* Title Text */}
                 <div>
-                    <h1 className="font-bold">
+                    <h1 className="font-bold text-4xl">
                         Why people love ChatCHW
                     </h1>
                 </div>
 
                 {/* Paragraph Text */}
-                <div>
-                    <p>
-                        At ChatCHW, we measure our success by the <br />
-                        positive outcomes achieved by CHWs and the <br />
-                        communities they serve. By equipping CHWs with <br />
-                        the resources and support they need, we aim to: <br />
+                <div className="pt-3">
+                    <p className="">
+                        At ChatCHW, we measure our success by the
+                        positive outcomes achieved by CHWs and the
+                        communities they serve. By equipping CHWs with
+                        the resources and support they need, we aim to:
                     </p>
                 </div>
 
                 {/* Bullet points */}
-                <div>
+                <div className="pt-3">
                     <ul className="list-outside">
-                        <li>Improve healthcare access and quality in underserved communities</li>
-                        <li>Reduce health disparities and imrpove health equity</li>
-                        <li>Enchance the overall well-being and health outcomes of individuals and families</li>
-                        <li>Strengthen community resilience and self-sufficiency</li>
+                        <li className="pt-2">Improve healthcare access and quality in underserved communities</li>
+                        <li className="pt-2">Reduce health disparities and imrpove health equity</li>
+                        <li className="pt-2">Enchance the overall well-being and health outcomes of individuals and families</li>
+                        <li className="pt-2">Strengthen community resilience and self-sufficiency</li>
                     </ul>
                 </div>
             </div>
