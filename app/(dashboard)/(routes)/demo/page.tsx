f1c706408550dcf10cbb0ef50d82746c42481c7b
Timothy Le <tle0279@gmail.com>
2023-10-31 14:46:53 -0700

    Created skeleton of demo-page

--- app/(dashboard)/(routes)/demo/page.tsx
+++ app/(dashboard)/(routes)/demo/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 const DemoPage = () => {
     return ( 
         <div>
-            Demo Page
+            {/* TITLE */}
+            <h1 className="border border-black text-center mt-5 font-bold text-[#505D68] text-2xl">
+                How ChatCHW Works
+            </h1>
+
+            {/* IMAGE */}
+            <div className="mt-5 mr-auto ml-auto border border-black w-[60%] h-[15rem] text-center">
+                IMAGE PLACEHOLDER
+            </div>
+
+            {/* THREE POINTS */}
+            <div className="mt-5 columns-3 text-center border border-black">
+                <div className="border border-black">
+                    Download
+                </div>
+                <div className="border border-black">
+                    Setup
+                </div>
+                <div className="border border-black">
+                    Use
+                </div>
+            </div>
+
         </div>
      );
 }
