8aa99b026c256f15b7c3f0896aa7f4e115ad5b5d
Timothy Le <tle0279@gmail.com>
2024-04-10 18:52:11 -0700

    Section 1-2 Mobile

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -26,21 +26,23 @@ const HomePage = () => {
       </div>
       
       {/* Second Portion */}
-      <div className ="w-full mb-32 "> 
+      <div className="w-full mb-32"> 
           <section className="font-ubuntu text-center px-36 pt-6 text-5xl font-bold text-gray-600">
             <h1> Facilitating Community</h1>
           </section>
 
-          <section className="text-gray-400 font-ubuntu text-center py-16 px-96 flex-wrap ">
-            <p>Delivering innovative solutions and fostering collaboration to achieve lasting positive impact and sustainable growth. 
+          <section className="flex text-gray-400 font-ubuntu text-center items-center px-36">
+            <p className="pt-8">
+              Delivering innovative solutions and fostering collaboration to achieve lasting positive impact and sustainable growth. 
               We offer a comprehensive platform that equips CHWs with cutting-edge technology and training, enabling them to deliver 
-              more efficient and effective care to their communities.</p>
+              more efficient and effective care to their communities.
+            </p>
           </section>
       </div> 
 
       {/* Third Portion */}
       <div className ="flex items-center justify-evenly"> 
-        <div className ="mr-20 ">
+        <div className ="hidden md:flex mr-20 ">
           <Image
             src = "/assets/nurse.svg"
             height ={547}
@@ -49,8 +51,10 @@ const HomePage = () => {
             className="bg-center bg-cover"
           />
         </div> 
-        <div className = "font-ubuntu align-middle flex-wrap px-32 w-1/2 overflow-wrap">
-          <h1 className = "text-3xl text-gray-800 font-bold	mb-6">  Who we are   </h1>
+        <div className = "font-ubuntu align-middle flex-wrap w-1/2 overflow-wrap">
+          <h1 className = "text-3xl text-gray-800 font-bold	mb-6"> 
+            Who we are   
+          </h1>
           <p className=" text-gray-500 mb-20">
             ChatCHW is dedicated to supporting and empowering community health workers (CHWs)
             in their crucial role within healthcare ecosystems by providing a platform to help
@@ -58,17 +62,20 @@ const HomePage = () => {
           </p>
           <Link
             href ="/about"
-            className="hover:bg-emerald-600 px-12 text-slate-50	py-3 rounded  bg-emerald-400	"
+            className="hover:bg-emerald-600 w-[30%] text-slate-50	py-3 rounded bg-emerald-400 flex border border-black"
           >
-            About Us
+            <p className="text-center ml-auto mr-auto">
+              About Us
+            </p>
           </Link>
           
         </div>
       </div>
 
       <div className = "flex items-center justify-evenly mb-32">
+        {/* FIX WRAP -> remove px-32 -> remove map: hidden */}
         <div className="font-ubuntu align-middle flex-wrap px-32 w-1/2 overflow-wrap">
-          <h1 className = "text-3xl text-gray-800 font-bold	mb-6">Problem we are solving</h1>
+          <h1 className = "text-3xl text-gray-800 font-bold	mb-6 sm:pt-20">Problem we are solving</h1>
           <p className=" text-gray-500 mb-20">We understand that CHWs play a vital role in bridging the gap between 
             underserved communities and healthcare services, and we are
              committed to providing them with the tools, resources,
@@ -76,7 +83,7 @@ const HomePage = () => {
           </p>
           <Link
             href ="/about"
-            className="hover:bg-emerald-600 px-12 text-slate-50	py-3 rounded  bg-emerald-400	"
+            className="hover:bg-emerald-600 px-12 text-slate-50	py-3 rounded bg-emerald-400	"
           >
             Learn More
           </Link>
@@ -87,14 +94,15 @@ const HomePage = () => {
             height ={600}
             width = {870}
             alt = "image"
-            className="bg-center bg-cover "
+            className="bg-center bg-cover hidden md:flex mr-20"
           />
         </div> 
        
       </div>
-
+      
       <div className="font-ubuntu align-middle mb-5">
         <h1 className="flex justify-center text-5xl font-bold mb-8 text-gray-700"> Features </h1>
+        {/* CHECK px-96, typo on text-gray-700 */}
         <section className="flex justify-center text-2xl font-bold px-96 text=grau=700">
           ChatCHW envisions healthier, more vibrant communities where everyone has the opportunity to thrive.
         </section>
@@ -102,6 +110,7 @@ const HomePage = () => {
 
       <div className="flex items-center justify-evenly mb-24"> 
         <div>
+          {/* CONVERT TO TEXT */}
           <Image 
             src="assets/steps.svg"
             width={375}
